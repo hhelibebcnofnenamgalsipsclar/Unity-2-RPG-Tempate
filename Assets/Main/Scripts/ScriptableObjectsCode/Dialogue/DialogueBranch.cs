fa1fbeb8de5de1c831092e30bde1f562e85d72ef
_ <=.@users.noreply.github.com>
2020-11-19 16:45:50 -0800

    hi
    hi

---
+++ Assets/Main/Scripts/ScriptableObjectsCode/Dialogue/DialogueBranch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Brach", menuName="CustomObject/Branch")]
+public class DialogueBranch : ScriptableObject
+{
+    public string dialogueID = "";
+    public List<string> DialogueLines;
+    public List<ResponseBranch> ResponseOptions;
+
+    // Start is called before the first frame update
+  
+}
+[System.Serializable]
+public class ResponseBranch
+{
+    public string text;
+    public DialogueBranch nextBranch;
+
+}
--- Assets/Main/Scripts/UI/DialogUIManager.cs
+++ Assets/Main/Scripts/UI/DialogUIManager.cs
@@ -17,6 +17,8 @@ public class DialogUIManager : MonoBehaviour
     [SerializeField]
     private int responses = 0;
 
+    private DialogueBranch branch;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -50,18 +52,46 @@ public class DialogUIManager : MonoBehaviour
     public void NextBranch(int branchSelect)
     {
         // Add ReciveDialogueBranch with newBranch being next branch
+        RecieveDialogueBranch(branch.ResponseOption[branchSelect].nextBranch);
+        ActiveDialogue();
+        NextDialogue(); 
     }
 
     
-    public void RecieveDialogueBranch()
+    public void RecieveDialogueBranch(DialogueBranch newBranch)
     {
         // Add branch info here
+        this.branch=newBranch;
+        responses=Mathf.Clamp(branch.ResponseOptions.Count,0,3);
+        currentIndex = 0;
+
     }
 
     public void NextDialogue()
     {
-        DeactiveDialogue(); // Remove this and
+       // DeactiveDialogue(); // Remove this and
        // add next Dialogue mechanism here
+       //if at end of branch
+       if(currentIndex>= branch.DialogueLines.Count);
+       {
+           if(responses ==0){
+               DeactiveDialogue();
+           }
+           else
+           {
+               continueText.SetActive(false);   
+               for(int i = 0; i<responses; i++)
+               {
+                   if (i>=3)
+                   {
+                        break;
+                   }
+                   responsesHolder[i].gameObject.SetActive(true);
+                   responsesHolder[i].GetComponentInChildren<TextMeshProUGUI>().text = branch.ResponseOption[i].text;
+
+               }
+           }
+       }
 
     }
 }
